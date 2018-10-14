64dabfbda86aacc9fbdc22e51ac44a1883606861
Robin Tram <robin.tram@gmail.com>
2018-10-14 16:19:58 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,8 +4,12 @@ class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def self.knowledge 
+    KNOWLEDGE
+  end
+
   def teach
-    
+    self.class.knowledge.sample
   end 
   
   
