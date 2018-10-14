b938f1a7b0b47cbb9c73adc5ff63a554b9fba57d
Robin Tram <robin.tram@gmail.com>
2018-10-14 16:16:15 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -4,11 +4,10 @@ class User
   
   attr_accessor :first_name, :last_name 
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize 
+
     
   end 
   
-  
+   
 end 
